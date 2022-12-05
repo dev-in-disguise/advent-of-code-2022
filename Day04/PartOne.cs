7d3abc93ddc366205bef501ab883f48f2a34310d
Manuel Zelenka <manuel.zelenka@gmail.com>
2022-12-05 09:09:02 +0100

    Solve day four

---
+++ Day04/PartOne.cs
@@ -0,0 +1,36 @@
+﻿namespace Day03;
+
+internal sealed class PartOne
+{
+    internal static void Run()
+    {
+        string[] pairings = InputProvider.GetInput().Split(Environment.NewLine);
+        int irrelevantElveCount = 0;
+
+        foreach(string pairing in pairings)
+        {
+            string[] elves = pairing.Split(',');
+
+            IEnumerable<int> elveOneSections = GetSections(elves[0]);
+            IEnumerable<int> elveTwoSections = GetSections(elves[1]);
+
+            int intersectCount = elveOneSections.Intersect(elveTwoSections).Count();
+
+            if(intersectCount == elveOneSections.Count() || intersectCount == elveTwoSections.Count())
+            {
+                irrelevantElveCount++;
+            }
+        }
+
+        Console.WriteLine(irrelevantElveCount);
+    }
+
+    private static IEnumerable<int> GetSections(string elveSections)
+    {
+        string[] fromTo = elveSections.Split('-');
+        int from = int.Parse(fromTo[0]);
+        int to = int.Parse(fromTo[1]);
+
+        return Enumerable.Range(from, to - from + 1);
+    }
+}
