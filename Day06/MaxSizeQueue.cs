8767065bbcac8f9881dd9e5ae9bf953b049759f6
Manuel Zelenka <manuel.zelenka@gmail.com>
2022-12-06 09:29:48 +0100

    Solve day six (#12)

---
+++ Day06/MaxSizeQueue.cs
@@ -0,0 +1,21 @@
+﻿namespace Day06;
+
+internal class MaxSizeQueue<T> : Queue<T>
+{
+    private readonly int maxCapacity;
+    
+    public MaxSizeQueue(int maxCapacity)
+    {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public new void Enqueue(T item)
+    {
+        if(this.Count == maxCapacity)
+        {
+            base.Dequeue();
+        }
+
+        base.Enqueue(item);
+    }
+}
---
+++ Day06/PartOne.cs
@@ -0,0 +1,29 @@
+﻿namespace Day06;
+
+internal sealed class PartOne
+{
+    internal static void Run()
+    {
+        string input = InputProvider.GetInput();
+        MaxSizeQueue<char> startMarker = new(4);
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            startMarker.Enqueue(input[i]);
+
+            if(IsStartOfPacketMarker(startMarker))
+            {
+                Console.WriteLine(i + 1);
+                break;
+            }
+        }
+    }
+
+    private static bool IsStartOfPacketMarker(MaxSizeQueue<char> startMarker)
+    {
+        if(startMarker.Count != 4) return false;
+
+        var distinct = startMarker.AsEnumerable().Distinct();
+        return distinct.Count() == startMarker.Count;
+    }
+}
