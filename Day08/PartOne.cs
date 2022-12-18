422638740ec6944c36cd5c83e06d96bebbbd3668
Manuel Zelenka <manuel.zelenka@gmail.com>
2022-12-18 10:08:54 +0100

    Solve day eight (#17)

---
+++ Day08/PartOne.cs
@@ -0,0 +1,86 @@
+﻿namespace Day08;
+
+internal sealed class PartOne
+{
+    internal static void Run()
+    {
+        string[] inputLines = InputProvider.GetInput().Split(Environment.NewLine);
+
+        int visibleTrees = 0;
+
+        for (int line = 0; line < inputLines.Length; line++)
+        {
+            for(int column = 0; column < inputLines[line].Length; column++)
+            {
+                if(IsVisible(line, column, inputLines))
+                {
+                    visibleTrees++;
+                }
+            }
+        }
+
+        Console.WriteLine(visibleTrees);
+    }
+
+    private static bool IsVisible(int lineIndex, int columnIndex, string[] inputLines)
+    {
+        int treeHeight = int.Parse(inputLines[lineIndex][columnIndex].ToString());
+
+        // check left
+        bool higherTreeExists = false;
+        if (columnIndex == 0) return true;
+        for(int leftIndex = columnIndex - 1; leftIndex >= 0; leftIndex--)
+        {
+            if (int.Parse(inputLines[lineIndex][leftIndex].ToString()) >= treeHeight)
+            {
+                higherTreeExists = true;
+                break;
+            }
+        }
+
+        if (!higherTreeExists) return true;
+
+        // check right
+        higherTreeExists = false;
+        if (columnIndex == inputLines[lineIndex].Length -1) return true;
+        for (int rightIndex = columnIndex + 1; rightIndex < inputLines[lineIndex].Length; rightIndex++)
+        {
+            if (int.Parse(inputLines[lineIndex][rightIndex].ToString()) >= treeHeight)
+            {
+                higherTreeExists = true;
+                break;
+            }
+        }
+
+        if (!higherTreeExists) return true;
+
+        // check top
+        higherTreeExists = false;
+        if (lineIndex == 0) return true;
+        for (int topIndex = lineIndex - 1; topIndex >= 0; topIndex--)
+        {
+            if (int.Parse(inputLines[topIndex][columnIndex].ToString()) >= treeHeight)
+            {
+                higherTreeExists = true;
+                break;
+            }
+        }
+
+        if (!higherTreeExists) return true;
+
+        // check bottom
+        higherTreeExists = false;
+        if (lineIndex == 0) return true;
+        for (int bottomIndex = lineIndex + 1; bottomIndex < inputLines.Length; bottomIndex++)
+        {
+            if (int.Parse(inputLines[bottomIndex][columnIndex].ToString()) >= treeHeight)
+            {
+                higherTreeExists = true;
+                break;
+            }
+        }
+
+        if (!higherTreeExists) return true;
+        else return false;
+    }
+}
---
+++ Day08/PartTwo.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+
+namespace Day08;
+
+internal sealed class PartTwo
+{
+    internal static void Run()
+    {
+        string[] inputLines = InputProvider.GetInput().Split(Environment.NewLine);
+
+        int highestScenicScore = 0;
+
+        for (int line = 0; line < inputLines.Length; line++)
+        {
+            for (int column = 0; column < inputLines[line].Length; column++)
+            {
+                int? scenicScore;
+                if ((scenicScore = GetScenicScore(line, column, inputLines)) > highestScenicScore)
+                {
+                    Debug.Assert(scenicScore is not null);
+                    highestScenicScore = scenicScore.Value;
+                }
+            }
+        }
+
+        Console.WriteLine(highestScenicScore);
+    }
+
+    private static int GetScenicScore(int lineIndex, int columnIndex, string[] inputLines)
+    {
+        int treeHeight = int.Parse(inputLines[lineIndex][columnIndex].ToString());
+
+        // check left
+        int scoreLeft = 0;
+        for (int leftIndex = columnIndex - 1; leftIndex >= 0; leftIndex--)
+        {
+            scoreLeft++;
+            int leftTreeHeight = int.Parse(inputLines[lineIndex][leftIndex].ToString());
+            if (leftTreeHeight >= treeHeight) break;
+        }
+
+
+        // check right
+        int scoreRight = 0;
+        for (int rightIndex = columnIndex + 1; rightIndex < inputLines[lineIndex].Length; rightIndex++)
+        {
+            scoreRight++;
+            int rightTreeHeight = int.Parse(inputLines[lineIndex][rightIndex].ToString());
+            if (rightTreeHeight >= treeHeight) break;
+        }
+
+        // check top
+        int scoreTop = 0;
+        for (int topIndex = lineIndex - 1; topIndex >= 0; topIndex--)
+        {
+            scoreTop++;
+            int topTreeHeight = int.Parse(inputLines[topIndex][columnIndex].ToString());
+            if (topTreeHeight >= treeHeight) break;
+        }
+
+        // check bottom
+        int scoreBottom = 0;
+        for (int bottomIndex = lineIndex + 1; bottomIndex < inputLines.Length; bottomIndex++)
+        {
+            scoreBottom++;
+            int bottomTreeHeight = int.Parse(inputLines[bottomIndex][columnIndex].ToString());
+            if (bottomTreeHeight >= treeHeight) break;
+        }
+
+        return scoreBottom * scoreTop * scoreLeft * scoreRight;
+    }
+}
